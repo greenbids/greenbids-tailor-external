eebe3d7b3330aaec3332f59a421ebbc1a565e139
Alexis BRENON <alexis@greenbids.ai>
2024-07-25 17:42:11 +0200

    refactor(python): rollback to Fabric-based API

--- python/src/greenbids/tailor/core/app/routers/root.py
+++ python/src/greenbids/tailor/core/app/routers/root.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from greenbids.tailor.core import models
+from greenbids.tailor.core import fabric
 from greenbids.tailor.core.app.resources import APP_RESOURCES
 
 router = APIRouter(tags=["Main"])
@@ -7,21 +7,25 @@ router = APIRouter(tags=["Main"])
 
 @router.put("/")
 async def get_buyers_probabilities(
-    fabrics: list[models.Model.GetBuyersProbabilitiesInput],
-) -> list[models.Model.GetBuyersProbabilitiesOutput]:
+    fabrics: list[fabric.Fabric],
+) -> list[fabric.Fabric]:
     """Compute the probability of the buyers to provide a bid.
 
     This must be called for each adcall.
+    Only the feature map attribute of the fabrics needs to be present.
+    The prediction attribute will be populated in the returned response.
     """
     return APP_RESOURCES.gb_model.get_buyers_probabilities(fabrics)
 
 
 @router.post("/")
 async def report_buyers_status(
-    fabrics: list[models.Model.ReportBuyersStatusInput],
-) -> list[models.Model.ReportBuyersStatusOutput]:
+    fabrics: list[fabric.Fabric],
+) -> list[fabric.Fabric]:
     """Train model according to actual outcome.
 
     This must NOT be called for each adcall, but only for exploration ones.
+    All fields of the fabrics need to be set.
+    Returns the same data than the input.
     """
     return APP_RESOURCES.gb_model.report_buyers_status(fabrics)
--- python/src/greenbids/tailor/core/models.py
+++ python/src/greenbids/tailor/core/models.py
@@ -7,34 +7,18 @@ from greenbids.tailor.core import fabric
 
 class Model(ABC):
 
-    class GetBuyersProbabilitiesInput(fabric.WithFeatureMap):
-        pass
-
-    class GetBuyersProbabilitiesOutput(fabric.WithPrediction, fabric.WithFeatureMap):
-        pass
-
     @abstractmethod
     def get_buyers_probabilities(
         self,
-        fabrics: list[GetBuyersProbabilitiesInput],
-    ) -> list[GetBuyersProbabilitiesOutput]:
+        fabrics: list[fabric.Fabric],
+    ) -> list[fabric.Fabric]:
         raise NotImplementedError
-
-    class ReportBuyersStatusInput(
-        fabric.WithGroundTruth, fabric.WithPrediction, fabric.WithFeatureMap
-    ):
-        pass
-
-    class ReportBuyersStatusOutput(
-        fabric.WithGroundTruth, fabric.WithPrediction, fabric.WithFeatureMap
-    ):
-        pass
 
     @abstractmethod
     def report_buyers_status(
         self,
-        fabrics: list[ReportBuyersStatusInput],
-    ) -> list[ReportBuyersStatusOutput]:
+        fabrics: list[fabric.Fabric],
+    ) -> list[fabric.Fabric]:
         raise NotImplementedError
 
 
@@ -42,15 +26,15 @@ class NullModel(Model):
 
     def get_buyers_probabilities(
         self,
-        fabrics: list[Model.GetBuyersProbabilitiesInput],
-    ) -> list[Model.GetBuyersProbabilitiesOutput]:
-        return [Model.GetBuyersProbabilitiesOutput(**f.model_dump()) for f in fabrics]
+        fabrics: list[fabric.Fabric],
+    ) -> list[fabric.Fabric]:
+        return fabrics
 
     def report_buyers_status(
         self,
-        fabrics: list[Model.ReportBuyersStatusInput],
-    ) -> list[Model.ReportBuyersStatusOutput]:
-        return [Model.ReportBuyersStatusOutput(**f.model_dump()) for f in fabrics]
+        fabrics: list[fabric.Fabric],
+    ) -> list[fabric.Fabric]:
+        return fabrics
 
 
 REGISTRY = entry_points(group="greenbids-tailor-models")
