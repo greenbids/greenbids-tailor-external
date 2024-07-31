6db3d97066ef2843f224cfdd5ef748cec5c67e38
Alexis BRENON <alexis@greenbids.ai>
2024-07-31 09:33:11 +0200

    refactor(python,telemetry): remove unused attributes

--- python/src/greenbids/tailor/core/telemetry.py
+++ python/src/greenbids/tailor/core/telemetry.py
@@ -2,32 +2,33 @@ import datetime
 import logging
 import os
 
-from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
-from opentelemetry.sdk import metrics, trace, _logs as logs
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import (
+    OTLPMetricExporter,
+)
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter,
+)
+from opentelemetry.sdk import _logs as logs
+from opentelemetry.sdk import metrics, trace
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.metrics.export import (
     PeriodicExportingMetricReader,
 )
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 
-from greenbids.tailor.core._version import __version__
-from greenbids.tailor.core.logging import RateLimitingFilter
-
-instrumentation_root = "greenbids.tailor"
+from .logging import RateLimitingFilter
 
 meter_provider = metrics.MeterProvider(
     metric_readers=([PeriodicExportingMetricReader(OTLPMetricExporter())])
 )
-meter = meter_provider.get_meter(instrumentation_root, __version__)
 
 tracer_provider = trace.TracerProvider()
 tracer_provider.add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
-tracer = tracer_provider.get_tracer(instrumentation_root, __version__)
 
 logger_provider = logs.LoggerProvider()
 logger_provider.add_log_record_processor(BatchLogRecordProcessor(OTLPLogExporter()))
+
 handler = logs.LoggingHandler(
     level=logging.getLevelNamesMapping()[
         # Only report error messages by default
