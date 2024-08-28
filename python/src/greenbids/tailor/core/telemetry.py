b0c2a14638125a71756c1dfd5489afe262252c49
Alexis BRENON <alexis@greenbids.ai>
2024-08-28 09:54:20 +0200

    feat(python): send error logs to backend

--- python/src/greenbids/tailor/core/telemetry.py
+++ python/src/greenbids/tailor/core/telemetry.py
@@ -1,10 +1,16 @@
+import logging
+import os
+
 from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk import metrics, trace
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+from opentelemetry.sdk import metrics, trace, _logs as logs
 from opentelemetry.sdk.metrics.export import (
     PeriodicExportingMetricReader,
 )
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+
 from greenbids.tailor.core._version import __version__
 
 meter_provider = metrics.MeterProvider(
@@ -12,8 +18,21 @@ meter_provider = metrics.MeterProvider(
 )
 tracer_provider = trace.TracerProvider()
 tracer_provider.add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
+logger_provider = logs.LoggerProvider()
+logger_provider.add_log_record_processor(BatchLogRecordProcessor(OTLPLogExporter()))
 
 instrumentation_root = "greenbids.tailor"
 
 meter = meter_provider.get_meter(instrumentation_root, __version__)
 tracer = tracer_provider.get_tracer(instrumentation_root, __version__)
+
+handler = logs.LoggingHandler(
+    level=logging.getLevelNamesMapping()[
+        # Only report error messages by default
+        os.environ.get("GREENBIDS_TAILOR_SUPPORT_LOG_LEVEL", "ERROR")
+    ],
+    logger_provider=logger_provider,
+)
+# Attach handler to root logger and greenbids logger (that doesn't propagate)
+logging.getLogger().addHandler(handler)
+logging.getLogger(instrumentation_root).addHandler(handler)
