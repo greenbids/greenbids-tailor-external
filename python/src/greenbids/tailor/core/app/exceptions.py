5d4da65756df4b088a3c7c1245932acf598cc928
Alexis BRENON <alexis@greenbids.ai>
2024-11-06 20:43:56 +0100

    Rollback to 5xx http code

--- python/src/greenbids/tailor/core/app/exceptions.py
+++ python/src/greenbids/tailor/core/app/exceptions.py
@@ -2,13 +2,20 @@ from fastapi import Request, HTTPException, status
 from greenbids.tailor.core.app import resources
 import typing
 
+import logging
+
+_logger = logging.getLogger(__name__)
+
 
 async def model_not_ready_handler(request: Request, exc: resources.ModelNotReady):
-    del request
+    if not (request.method == "GET" and "healthz" in request.url.path):
+        _logger.warning(
+            "An access to the prediction model has been made, while it is not ready yet."
+        )
     raise HTTPException(
-        status_code=status.HTTP_425_TOO_EARLY,
+        status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
         detail="Model is not ready yet. Check readiness on route /healthz/readiness. Retry in few seconds.",
-        headers={"gb-tailor-model-name": exc.model_name},
+        headers={"gb-tailor-model-name": exc.model_name, "Retry-After": str(15)},
     )
 
 
